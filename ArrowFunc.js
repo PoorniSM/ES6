8ed3f7ed57df782005c5f926a34e74781b5e62d7
PoorniSM <85600279+PoorniSM@users.noreply.github.com>
2021-06-09 12:37:28 +0530

    Update ArrowFunc.js

--- ArrowFunc.js
+++ ArrowFunc.js
@@ -4,13 +4,13 @@ const multiply = numbers.map( num => num*2 );
 console.log('Numbers multiplied by 2',multiply);
 
 //Normal function
-function Message1(animal){
+function message1(animal){
     return `${animal} makes noise`
 }
-console.log(Message1('Dog'));
+console.log(message1('Dog'));
 
 //Arrow function
-const Message2 = (animal) => {
+const message2 = (animal) => {
     return `${animal} makes noise`
 }
-console.log(Message2('Cat'));
+console.log(message2('Cat'));
